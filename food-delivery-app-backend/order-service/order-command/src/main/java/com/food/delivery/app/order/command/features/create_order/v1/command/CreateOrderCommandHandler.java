0b82c5f8fca525f45bf96d8cc6a76c2bfef87a5f
mustardpenguin <andyha234@gmail.com>
2024-10-31 14:13:31 -0400

    Circuit breaking for calling restaurant grpc service

--- food-delivery-app-backend/order-service/order-command/src/main/java/com/food/delivery/app/order/command/features/create_order/v1/command/CreateOrderCommandHandler.java
+++ food-delivery-app-backend/order-service/order-command/src/main/java/com/food/delivery/app/order/command/features/create_order/v1/command/CreateOrderCommandHandler.java
@@ -14,6 +14,7 @@ import com.food.delivery.app.order.command.shared.repository.order.OrderJpaRepos
 import com.food.delivery.app.common.utility.datetime.DateTimeUtil;
 import com.food.delivery.app.common.utility.objectmapper.ObjectMapperUtil;
 import com.food.delivery.app.order.command.shared.util.GetProductsUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
--- food-delivery-app-backend/order-service/order-command/src/main/java/com/food/delivery/app/order/command/shared/util/GetProductsUtil.java
+++ food-delivery-app-backend/order-service/order-command/src/main/java/com/food/delivery/app/order/command/shared/util/GetProductsUtil.java
@@ -3,6 +3,9 @@ package com.food.delivery.app.order.command.shared.util;
 import com.food.delivery.app.common.proto.ProductServiceGrpc;
 import com.food.delivery.app.common.proto.ProductServiceProto;
 import com.food.delivery.app.order.command.domain.valueobjects.Product;
+import com.food.delivery.app.order.command.shared.exceptions.OrderException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +13,14 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Component
 public class GetProductsUtil {
 
     @GrpcClient("restaurant-service")
     private ProductServiceGrpc.ProductServiceBlockingStub productServiceStub;
 
+    @CircuitBreaker(name = "grpc-circuit-breaker", fallbackMethod = "handleFallback")
     public List<Product> getProductsByRestaurantId(UUID restaurantId) {
 
         ProductServiceProto.ProductRequest request = ProductServiceProto.ProductRequest.newBuilder()
@@ -33,5 +38,10 @@ public class GetProductsUtil {
                 .price(BigDecimal.valueOf(product.getPrice()))
                 .available(product.getAvailable())
                 .build();
+    }
+
+    private List<Product> handleFallback(Throwable throwable) throws Throwable {
+        log.error("Error while calling restaurant grpc service: {}", throwable.getMessage());
+        throw new OrderException("Error while calling restaurant grpc service: " + throwable.getMessage());
     }
 }
