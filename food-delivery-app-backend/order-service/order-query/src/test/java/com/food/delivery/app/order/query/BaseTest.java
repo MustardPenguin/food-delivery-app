e2d62077116336726357edbae3bedcbbbaf6e30d
mustardpenguin <andyha234@gmail.com>
2024-11-04 15:35:40 -0500

    Fixed tests

--- food-delivery-app-backend/order-service/order-query/src/test/java/com/food/delivery/app/order/query/BaseTest.java
+++ food-delivery-app-backend/order-service/order-query/src/test/java/com/food/delivery/app/order/query/BaseTest.java
@@ -1,14 +1,39 @@
 package com.food.delivery.app.order.query;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
+import java.time.Duration;
+
 public class BaseTest {
 
     @Container
-    private static final ElasticsearchContainer container = new ElasticsearchContainer(
-            DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.7.0")) //
+    private static final ElasticsearchContainer elasticSearchContainer = new ElasticsearchContainer(
+            DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.7.0"))
+            .withEnv("xpack.security.enabled", "false")
             .withReuse(true);
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.elasticsearch.uris", elasticSearchContainer::getHttpHostAddress);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        elasticSearchContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        elasticSearchContainer.stop();
+    }
 }
--- food-delivery-app-backend/order-service/order-query/src/test/java/com/food/delivery/app/order/query/OrderCreatedTest.java
+++ food-delivery-app-backend/order-service/order-query/src/test/java/com/food/delivery/app/order/query/OrderCreatedTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.internal.matchers.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.math.BigDecimal;
 import java.time.Instant;
