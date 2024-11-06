7720c9ae56fab72b9a5f6d2c416ff4732dc02d6f
mustardpenguin <andyha234@gmail.com>
2024-11-06 18:40:52 -0500

    updated project directories

---
+++ food-delivery-app-backend/java/common/proto/target/generated-sources/protobuf/grpc-java/com/food/delivery/app/common/proto/ProductServiceGrpc.java
@@ -0,0 +1,293 @@
+package com.food.delivery.app.common.proto;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.63.0)",
+    comments = "Source: ProductService.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class ProductServiceGrpc {
+
+  private ProductServiceGrpc() {}
+
+  public static final java.lang.String SERVICE_NAME = "com.food.delivery.app.common.proto.ProductService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest,
+      com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse> getGetProductsByRestaurantIdMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "GetProductsByRestaurantId",
+      requestType = com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest.class,
+      responseType = com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest,
+      com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse> getGetProductsByRestaurantIdMethod() {
+    io.grpc.MethodDescriptor<com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest, com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse> getGetProductsByRestaurantIdMethod;
+    if ((getGetProductsByRestaurantIdMethod = ProductServiceGrpc.getGetProductsByRestaurantIdMethod) == null) {
+      synchronized (ProductServiceGrpc.class) {
+        if ((getGetProductsByRestaurantIdMethod = ProductServiceGrpc.getGetProductsByRestaurantIdMethod) == null) {
+          ProductServiceGrpc.getGetProductsByRestaurantIdMethod = getGetProductsByRestaurantIdMethod =
+              io.grpc.MethodDescriptor.<com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest, com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "GetProductsByRestaurantId"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse.getDefaultInstance()))
+              .setSchemaDescriptor(new ProductServiceMethodDescriptorSupplier("GetProductsByRestaurantId"))
+              .build();
+        }
+      }
+    }
+    return getGetProductsByRestaurantIdMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static ProductServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<ProductServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<ProductServiceStub>() {
+        @java.lang.Override
+        public ProductServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new ProductServiceStub(channel, callOptions);
+        }
+      };
+    return ProductServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static ProductServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<ProductServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<ProductServiceBlockingStub>() {
+        @java.lang.Override
+        public ProductServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new ProductServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return ProductServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static ProductServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<ProductServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<ProductServiceFutureStub>() {
+        @java.lang.Override
+        public ProductServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new ProductServiceFutureStub(channel, callOptions);
+        }
+      };
+    return ProductServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public interface AsyncService {
+
+    /**
+     */
+    default void getProductsByRestaurantId(com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest request,
+        io.grpc.stub.StreamObserver<com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse> responseObserver) {
+      io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getGetProductsByRestaurantIdMethod(), responseObserver);
+    }
+  }
+
+  /**
+   * Base class for the server implementation of the service ProductService.
+   */
+  public static abstract class ProductServiceImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return ProductServiceGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service ProductService.
+   */
+  public static final class ProductServiceStub
+      extends io.grpc.stub.AbstractAsyncStub<ProductServiceStub> {
+    private ProductServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected ProductServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new ProductServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void getProductsByRestaurantId(com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest request,
+        io.grpc.stub.StreamObserver<com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse> responseObserver) {
+      io.grpc.stub.ClientCalls.asyncUnaryCall(
+          getChannel().newCall(getGetProductsByRestaurantIdMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do synchronous rpc calls to service ProductService.
+   */
+  public static final class ProductServiceBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<ProductServiceBlockingStub> {
+    private ProductServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected ProductServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new ProductServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse getProductsByRestaurantId(com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getGetProductsByRestaurantIdMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service ProductService.
+   */
+  public static final class ProductServiceFutureStub
+      extends io.grpc.stub.AbstractFutureStub<ProductServiceFutureStub> {
+    private ProductServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected ProductServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new ProductServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse> getProductsByRestaurantId(
+        com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest request) {
+      return io.grpc.stub.ClientCalls.futureUnaryCall(
+          getChannel().newCall(getGetProductsByRestaurantIdMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_GET_PRODUCTS_BY_RESTAURANT_ID = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AsyncService serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_GET_PRODUCTS_BY_RESTAURANT_ID:
+          serviceImpl.getProductsByRestaurantId((com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest) request,
+              (io.grpc.stub.StreamObserver<com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getGetProductsByRestaurantIdMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              com.food.delivery.app.common.proto.ProductServiceProto.ProductRequest,
+              com.food.delivery.app.common.proto.ProductServiceProto.ProductResponse>(
+                service, METHODID_GET_PRODUCTS_BY_RESTAURANT_ID)))
+        .build();
+  }
+
+  private static abstract class ProductServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    ProductServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return com.food.delivery.app.common.proto.ProductServiceProto.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("ProductService");
+    }
+  }
+
+  private static final class ProductServiceFileDescriptorSupplier
+      extends ProductServiceBaseDescriptorSupplier {
+    ProductServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class ProductServiceMethodDescriptorSupplier
+      extends ProductServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final java.lang.String methodName;
+
+    ProductServiceMethodDescriptorSupplier(java.lang.String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (ProductServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new ProductServiceFileDescriptorSupplier())
+              .addMethod(getGetProductsByRestaurantIdMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
